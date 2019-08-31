4c94317112dc5fc3ed8353317c3934b93703daaa
Eugene Klimov <tirava@gmail.com>
2019-08-31 12:43:49 +0300

    HW-1 added mutex

--- homework-1/search/main.go
+++ homework-1/search/main.go
@@ -24,7 +24,9 @@ func main() {
 		"https://knizhnik.org/dmitrij-gluhovskij/metro-2033/1",
 	}
 
-	search := "Бим"
+	//search := "Бим"
+	//search := "Книга"
+	search := "книга"
 	//search := "1973"
 	//search := "2033"
 
@@ -34,40 +36,30 @@ func main() {
 func searchStringURL(search string, urls []string) (res []string) {
 
 	wg := &sync.WaitGroup{}
+	mux := &sync.Mutex{}
 
 	for _, url := range urls {
-		//resp, err := http.Get(url)
-		//if err != nil {
-		//	log.Printf("Error getting url: %v", err)
-		//	continue
-		//}
-		//body, err := ioutil.ReadAll(resp.Body)
-		//if err != nil {
-		//	log.Printf("Error reading body: %v", err)
-		//	continue
-		//}
-		//if strings.Contains(string(body), search) {
-		//	res = append(res, url)
-		//}
-		//_ = resp.Body.Close()
-
 		wg.Add(1)
 		go func(url string) {
 			defer wg.Done()
+
 			resp, err := http.Get(url)
 			if err != nil {
 				log.Printf("Error getting url: %v", err)
 				return
 			}
+			defer resp.Body.Close()
+
 			body, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
 				log.Printf("Error reading body: %v", err)
 				return
 			}
 			if strings.Contains(string(body), search) {
+				mux.Lock()
 				res = append(res, url)
+				mux.Unlock()
 			}
-			_ = resp.Body.Close()
 		}(url)
 	}
 
