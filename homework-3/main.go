bb23874f49367fcb253e6fd5a1de22d525ba80dc
Eugene Klimov <tirava@gmail.com>
2019-09-17 17:49:32 +0300

    HW-3 - fix path separators

--- homework-3/main.go
+++ homework-3/main.go
@@ -15,6 +15,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"path"
 	"path/filepath"
 	"strings"
 	"syscall"
@@ -22,9 +23,10 @@ import (
 
 const (
 	servAddr      = "localhost:8080"
-	indexTemplate = "templates/index.gohtml"
-	postTemplate  = "templates/post.gohtml"
-	postsPath     = "posts/"
+	indexTemplate = "index.gohtml"
+	postTemplate  = "post.gohtml"
+	templatePath  = "templates" // + string(os.PathSeparator)
+	postsPath     = "posts"     // + string(os.PathSeparator)
 	postsExt      = ".md"
 )
 
@@ -40,9 +42,11 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path[1:] == "" { // root handler
 		posts, err := getPosts()
 		if err != nil {
-			// todo - return w code 500
+			log.Println("error getting posts:", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
-		t := template.Must(template.ParseFiles(indexTemplate))
+		t := template.Must(template.ParseFiles(path.Join(templatePath, indexTemplate)))
 		//
 		err = t.Execute(w, posts)
 		if err != nil {
@@ -50,23 +54,26 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	f := postsPath + r.URL.Path[1:] + postsExt
+	f := path.Join(postsPath, r.URL.Path[1:]) + postsExt
 	rf, err := ioutil.ReadFile(f)
 	if err != nil {
-		//todo return error for bad file? & return w code
-		log.Println(err)
+		log.Println("error reading file:", err)
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+	//---------------
 	lines := strings.Split(string(rf), "\n")
-	title := string(lines[0])
-	date := string(lines[1])
-	summary := string(lines[2])
+	title, date, summary := lines[0], lines[1], lines[2]
 	body := strings.Join(lines[3:], "\n")
+	//---------------
 	post := Post{title, date, summary, body, r.URL.Path[1:]}
+	//---------------
 
-	t := template.Must(template.New("post.gohtml").Funcs(template.FuncMap{"markDown": markDowner}).ParseFiles(postTemplate))
+	t := template.Must(template.New(postTemplate).Funcs(template.FuncMap{
+		"markDown": markDowner,
+	}).ParseFiles(path.Join(templatePath, postTemplate)))
+
 	err = t.Execute(w, post)
-
 	if err != nil {
 		// todo buffer & return w code
 		log.Println(err)
@@ -80,22 +87,24 @@ func markDowner(args ...interface{}) template.HTML {
 
 func getPosts() ([]Post, error) {
 	posts := &[]Post{}
-	files, err := filepath.Glob(postsPath + "*" + postsExt)
+	files, err := filepath.Glob(path.Join(postsPath, "*") + postsExt)
 	if err != nil {
 		return nil, err
 	}
 	for _, f := range files {
-		file := strings.ReplaceAll(f, postsPath, "")  // remove path
-		file = strings.ReplaceAll(file, postsExt, "") // remove ext
+		file := strings.ReplaceAll(f, postsPath+string(os.PathSeparator), "") // remove path
+		file = strings.ReplaceAll(file, postsExt, "")                         // remove ext
 		rf, err := ioutil.ReadFile(f)
 		if err != nil {
 			continue // skip bad file
 		}
+		//--------------
 		lines := strings.Split(string(rf), "\n")
 		title, date, summary := lines[0], lines[1], lines[2]
 		body := strings.Join(lines[3:], "\n")
-		//body = string(blackfriday.Run([]byte(body)))
+		//--------------
 		*posts = append(*posts, Post{title, date, summary, body, file})
+		//--------------
 	}
 	return *posts, nil
 }
