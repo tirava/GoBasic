c3910a6a5136a96ed3c50c5aa833dd760f053c3b
Eugene Klimov <tirava@gmail.com>
2019-09-17 14:00:15 +0300

    HW-3 - draft markdown

--- homework-3/main.go
+++ homework-3/main.go
@@ -8,7 +8,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/russross/blackfriday"
+	"gopkg.in/russross/blackfriday.v2"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -23,7 +23,9 @@ import (
 const (
 	servAddr      = "localhost:8080"
 	indexTemplate = "templates/index.gohtml"
+	postTemplate  = "templates/post.gohtml"
 	postsPath     = "posts/"
+	postsExt      = ".md"
 )
 
 type Post struct {
@@ -35,27 +37,67 @@ type Post struct {
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	posts := getPosts()
-	t := template.Must(template.ParseFiles(indexTemplate))
-	t.Execute(w, posts)
+	if r.URL.Path[1:] == "" { // root handler
+		posts, err := getPosts()
+		if err != nil {
+			// todo - return w code 500
+		}
+		t := template.Must(template.ParseFiles(indexTemplate))
+		//
+		err = t.Execute(w, posts)
+		if err != nil {
+			// todo buffer
+		}
+		return
+	}
+	f := postsPath + r.URL.Path[1:] + postsExt
+	rf, err := ioutil.ReadFile(f)
+	if err != nil {
+		//todo return error for bad file? & return w code
+		log.Println(err)
+		return
+	}
+	lines := strings.Split(string(rf), "\n")
+	title := string(lines[0])
+	date := string(lines[1])
+	summary := string(lines[2])
+	body := strings.Join(lines[3:], "\n")
+	post := Post{title, date, summary, body, r.URL.Path[1:]}
+
+	t := template.Must(template.New("post.gohtml").Funcs(template.FuncMap{"markDown": markDowner}).ParseFiles(postTemplate))
+	err = t.Execute(w, post)
+
+	if err != nil {
+		// todo buffer & return w code
+		log.Println(err)
+	}
 }
 
-func getPosts() []Post {
+func markDowner(args ...interface{}) template.HTML {
+	s := blackfriday.Run([]byte(fmt.Sprintf("%s", args...)))
+	return template.HTML(s)
+}
+
+func getPosts() ([]Post, error) {
 	posts := &[]Post{}
-	files, _ := filepath.Glob(postsPath + "*")
+	files, err := filepath.Glob(postsPath + "*" + postsExt)
+	if err != nil {
+		return nil, err
+	}
 	for _, f := range files {
-		file := strings.ReplaceAll(f, postsPath, "") // remove path
-		file = strings.ReplaceAll(file, ".md", "")   // remove ext
-		rf, _ := ioutil.ReadFile(f)
+		file := strings.ReplaceAll(f, postsPath, "")  // remove path
+		file = strings.ReplaceAll(file, postsExt, "") // remove ext
+		rf, err := ioutil.ReadFile(f)
+		if err != nil {
+			continue // skip bad file
+		}
 		lines := strings.Split(string(rf), "\n")
-		title := string(lines[0])
-		date := string(lines[1])
-		summary := string(lines[2])
-		body := strings.Join(lines[3:len(lines)], "\n")
-		body = string(blackfriday.MarkdownCommon([]byte(body)))
+		title, date, summary := lines[0], lines[1], lines[2]
+		body := strings.Join(lines[3:], "\n")
+		//body = string(blackfriday.Run([]byte(body)))
 		*posts = append(*posts, Post{title, date, summary, body, file})
 	}
-	return *posts
+	return *posts, nil
 }
 
 func main() {
