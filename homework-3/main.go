640d6707d82b4ddf32938be3bdda2c427b8f0994
Eugene Klimov <tirava@gmail.com>
2019-09-17 23:38:17 +0300

    HW-3 - removed posts files

--- homework-3/main.go
+++ homework-3/main.go
@@ -9,16 +9,13 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi"
-	"gopkg.in/russross/blackfriday.v2"
+	//"gopkg.in/russross/blackfriday.v2"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"path"
-	"path/filepath"
-	"strings"
 	"syscall"
 )
 
@@ -26,80 +23,104 @@ const (
 	servAddr      = "localhost:8080"
 	indexTemplate = "index.gohtml"
 	postTemplate  = "post.gohtml"
-	templatePath  = "templates" // + string(os.PathSeparator)
-	postsPath     = "posts"     // + string(os.PathSeparator)
-	postsExt      = ".md"
+	templatePath  = "templates"
+	//postsPath     = "posts"
+	//postsExt      = ".md"
 )
 
+// Post is the base post type
 type Post struct {
 	Title   string
 	Date    string
 	Summary string
 	Body    template.HTML
-	File    string
+}
+
+// Posts storage todo - need SQL storage instead map
+var Posts map[string]Post
+
+func init() {
+	Posts = map[string]Post{
+		"first post": {
+			"111",
+			"222",
+			"333",
+			"444",
+		},
+		"second post": {
+			"555",
+			"666",
+			"777",
+			"888",
+		},
+	}
 }
 
 func mainPage(w http.ResponseWriter, _ *http.Request) {
-	posts, err := getPosts()
-	if err != nil {
-		log.Println("error getting posts:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	//posts := getPosts()
+	//if err != nil {
+	//	log.Println("error getting posts:", err)
+	//	w.WriteHeader(http.StatusInternalServerError)
+	//	return
+	//}
 	t := template.Must(template.ParseFiles(path.Join(templatePath, indexTemplate)))
-	err = t.Execute(w, posts)
+	err := t.Execute(w, Posts)
 	if err != nil {
 		// todo buffer & return w code
+		log.Println("error executing template in mainPage:", err)
 	}
 	return
 }
 
 func postPage(w http.ResponseWriter, r *http.Request) {
-	f := path.Join(postsPath, r.URL.Path[1:]) + postsExt
-	rf, err := ioutil.ReadFile(f)
-	if err != nil {
-		log.Println("error reading file:", err)
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	post := lines2Posts(rf, r.URL.Path[1:])
+	//f := path.Join(postsPath, r.URL.Path[1:]) + postsExt
+	//rf, err := ioutil.ReadFile(f)
+	//if err != nil {
+	//	log.Println("error reading file:", err)
+	//	w.WriteHeader(http.StatusNotFound)
+	//	return
+	//}
+	//post := lines2Posts(rf)
+	// todo check if r.URL.Path[1:] exists?
 	t := template.Must(template.ParseFiles(path.Join(templatePath, postTemplate)))
-	err = t.Execute(w, post)
+	err := t.Execute(w, Posts[r.URL.Path[1:]])
 	if err != nil {
 		// todo buffer & return w code
-		log.Println(err)
+		log.Println("error executing template in postPage:", err)
 	}
 }
 
-func getPosts() ([]Post, error) {
-	var posts []Post
-	files, err := filepath.Glob(path.Join(postsPath, "*") + postsExt)
-	if err != nil {
-		return nil, err
-	}
-	for _, f := range files {
-		file := strings.ReplaceAll(f, postsPath+string(os.PathSeparator), "") // remove path
-		file = strings.ReplaceAll(file, postsExt, "")                         // remove ext
-		rf, err := ioutil.ReadFile(f)
-		if err != nil {
-			continue // skip bad file
-		}
-		posts = append(posts, lines2Posts(rf, file))
-	}
-	return posts, nil
-}
+//func getPosts() []Post {
+//	var posts []Post
+//	//files, err := filepath.Glob(path.Join(postsPath, "*") + postsExt)
+//	//if err != nil {
+//	//	return nil, err
+//	//}
+//
+//	for _, post := range Posts {
+//		//file := strings.ReplaceAll(f, postsPath+string(os.PathSeparator), "") // remove path
+//		//file = strings.ReplaceAll(file, postsExt, "")                         // remove ext
+//		//rf, err := ioutil.ReadFile(f)
+//		//if err != nil {
+//		//	continue // skip bad file
+//		//}
+//		//posts = append(posts, lines2Posts(post))
+//		posts = append(posts, post)
+//	}
+//	return posts
+//}
 
-func lines2Posts(readFile []byte, file string) Post {
-	lines := strings.Split(string(readFile), "\n")
-	title, date, summary := lines[0], lines[1], lines[2]
-	body := template.HTML(strings.Join(lines[3:], "\n"))
-	post := Post{
-		title, date, summary,
-		template.HTML(blackfriday.Run([]byte(body))),
-		file,
-	}
-	return post
-}
+//func lines2Posts(readFile Post) Post {
+//	lines := strings.Split(string(readFile), "\n")
+//	title, date, summary := lines[0], lines[1], lines[2]
+//	body := template.HTML(strings.Join(lines[3:], "\n"))
+//	post := Post{
+//		title, date, summary,
+//		template.HTML(blackfriday.Run([]byte(body))),
+//		//title, // todo - use short name
+//	}
+//	return post
+//}
 
 func main() {
 
