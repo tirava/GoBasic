cbf4ae4270646eb26c036c4806f67a18bfa29eab
Eugene Klimov <tirava@gmail.com>
2019-09-21 22:39:35 +0300

    HW-4 - start

---
+++ homework-4/handlers.go
@@ -0,0 +1,157 @@
+/*
+ * HomeWork-3: Simple blog
+ * Created on 19.09.2019 19:35
+ * Copyright (c) 2019 - Eugene Klimov
+ */
+
+package main
+
+import (
+	"bytes"
+	"github.com/go-chi/chi"
+	"gopkg.in/russross/blackfriday.v2"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func (h *Handler) mainPageForm(w http.ResponseWriter, _ *http.Request) {
+	var b bytes.Buffer // no need to show bad content
+	if err := h.tmplGlob.ExecuteTemplate(&b, "index", h.posts); err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	if _, err := b.WriteTo(w); err != nil {
+		log.Println("can't write to ResponseWriter in indexPage")
+	}
+}
+
+func (h *Handler) postPageForm(w http.ResponseWriter, r *http.Request) {
+	postNum := chi.URLParam(r, "id")
+	if _, ok := h.posts[postNum]; !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	post := h.posts[postNum] // create copy for markdown convert
+	post.Body = template.HTML(blackfriday.Run([]byte(post.Body)))
+	var b bytes.Buffer // no need to show bad content
+	if err := h.tmplGlob.ExecuteTemplate(&b, "post", post); err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	if _, err := b.WriteTo(w); err != nil {
+		log.Println("can't write to ResponseWriter in postPage")
+	}
+}
+
+func (h *Handler) editPostPageForm(w http.ResponseWriter, r *http.Request) {
+	postNum := chi.URLParam(r, "id")
+	var b bytes.Buffer // no need to show bad content
+	if err := h.tmplGlob.ExecuteTemplate(&b, "edit", h.posts[postNum]); err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	if _, err := b.WriteTo(w); err != nil {
+		log.Println("can't write to ResponseWriter in editPostPageForm")
+	}
+}
+
+func (h *Handler) editPostPage(w http.ResponseWriter, r *http.Request) {
+	postNum := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(postNum)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	post := Post{
+		ID:      id,
+		Title:   r.FormValue("title"),
+		Date:    r.FormValue("date"),
+		Summary: r.FormValue("summary"),
+		Body:    template.HTML(r.FormValue("body")),
+	}
+	h.posts[postNum] = post
+	http.Redirect(w, r, postsURL+"/"+postNum, http.StatusFound)
+}
+
+func (h *Handler) createPostPageForm(w http.ResponseWriter, r *http.Request) {
+	var b bytes.Buffer // no need to show bad content
+	post := Post{}
+	if err := h.tmplGlob.ExecuteTemplate(&b, "create", post); err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	if _, err := b.WriteTo(w); err != nil {
+		log.Println("can't write to ResponseWriter in createPostPageForm")
+	}
+}
+
+func (h *Handler) createPostPage(w http.ResponseWriter, r *http.Request) {
+	id := h.nextGlobID()
+	ids := strconv.Itoa(id)
+	h.posts[ids] = Post{
+		ID:      id,
+		Title:   r.FormValue("title"),
+		Date:    r.FormValue("date"),
+		Summary: r.FormValue("summary"),
+		Body:    template.HTML(r.FormValue("body")),
+	}
+	http.Redirect(w, r, postsURL+"/"+ids, http.StatusFound)
+}
+
+func (h *Handler) deletePostPage(w http.ResponseWriter, r *http.Request) {
+	delete(h.posts, chi.URLParam(r, "id"))
+	http.Redirect(w, r, postsURL+"/", http.StatusFound)
+}
+
+func (h *Handler) nextGlobID() int {
+	h.mux.Lock()
+	h.globID++
+	h.mux.Unlock()
+	return h.globID
+}
+
+func (h *Handler) initPosts() {
+	h.posts = dbPosts{
+		"1": {
+			ID:      1,
+			Title:   "Мой первый пост!",
+			Date:    "18-е Сентября 2019 года",
+			Summary: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio praesentium, quos. Aspernatur assumenda cupiditate deserunt ducimus, eveniet, expedita inventore laboriosam magni modi non odio, officia qui sequi similique unde voluptatem.",
+			Body: `
+Здесь основной текст.
+# Markdown!
+*Это* **круто**!
+`,
+		},
+		"2": {
+			ID:    2,
+			Title: "Это уже второй пост!",
+			Date:  "19-е Сентября 2019 года",
+			Summary: `
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio praesentium, quos. Aspernatur assumenda cupiditate deserunt ducimus, eveniet, expedita inventore laboriosam magni modi non odio, officia qui sequi similique unde voluptatem.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio praesentium, quos. Aspernatur assumenda cupiditate deserunt ducimus, eveniet, expedita inventore laboriosam magni modi non odio, officia qui sequi similique unde voluptatem.
+`,
+			Body: `
+Разобрался в шаблонах и маркдаунах, как их совместить.
+
+Теперь понять, как переходить на отдельные посты.
+# Anybody!
+*Hz* **cool**!
+`,
+		},
+		"3": {
+			ID:      3,
+			Title:   "Пример основных вариантов разметки Markdown",
+			Date:    "20-е Сентября 2019 года",
+			Summary: "Официальное руководство по синтаксису Markdown мне кажется слишком длинным и не слишком наглядным, поэтому я составил краткое руководство, которое поможет выучить или повторить синтаксис Маркдауна за час.",
+			Body:    `todo`,
+		},
+	}
+	h.globID = 3 // last id
+}
---
+++ homework-4/main.go
@@ -0,0 +1,110 @@
+/*
+ * HomeWork-3: Simple blog
+ * Created on 18.09.2019 23:11
+ * Copyright (c) 2019 - Eugene Klimov
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"path"
+	"sync"
+	"syscall"
+)
+
+const (
+	servAddr     = ":8080"
+	templateExt  = "*.gohtml"
+	templatePath = "templates"
+	postsURL     = "/posts"
+	editURL      = "/edit"
+	deleteURL    = "/delete"
+	createURL    = "/create"
+	apiURL       = "/api/v1"
+	staticPath   = "/static"
+)
+
+// Post is the base post type
+type Post struct {
+	ID      int
+	Title   string
+	Date    string // todo change to time.Time
+	Summary string
+	Body    template.HTML
+}
+
+type dbPosts map[string]Post
+
+// Handler is the global handlers struct
+type Handler struct {
+	posts    dbPosts
+	tmplGlob *template.Template
+	globID   int
+	mux      sync.Mutex
+}
+
+func main() {
+
+	// new handler struct
+	handlers := &Handler{
+		tmplGlob: template.Must(template.ParseGlob(path.Join(templatePath, templateExt))),
+	}
+	handlers.initPosts()
+
+	// prepare routes & middleware
+	mux := chi.NewRouter()
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Get("/", handlers.mainPageForm)
+	mux.Route(apiURL, func(r chi.Router) {
+		r.Post(createURL, handlers.createPostPage)
+		r.Post(editURL+"/{id}", handlers.editPostPage)
+		r.Post(deleteURL+"/{id}", handlers.deletePostPage)
+	})
+	mux.Route(postsURL, func(r chi.Router) {
+		r.Get("/{id}", handlers.postPageForm)
+		r.Get("/", handlers.mainPageForm)
+		//r.Route(deleteURL, func(r chi.Router) {
+		//	r.Post("/{id}", handlers.deletePostPage)
+		//})
+		r.Route(createURL, func(r chi.Router) {
+			r.Get("/", handlers.createPostPageForm)
+			//r.Post("/", handlers.createPostPage)
+		})
+		r.Route(editURL, func(r chi.Router) {
+			r.Get("/{id}", handlers.editPostPageForm)
+			//r.Post("/{id}", handlers.editPostPage)
+		})
+	})
+
+	// custom server is for custom parameters & graceful shutdown
+	srv := &http.Server{Addr: servAddr, Handler: mux}
+
+	// static files
+	mux.Handle(staticPath+"/*", http.StripPrefix(staticPath, http.FileServer(http.Dir("."+staticPath))))
+
+	// graceful shutdown
+	shutdown := make(chan os.Signal)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM) // os.Kill cannot be trapped anyway!
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		log.Println("Signal received:", <-shutdown)
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Println("Error while shutdown server:", err)
+		}
+	}()
+
+	fmt.Println("Starting server at:", servAddr)
+	log.Printf("Shutdown server at: %s\n%v", servAddr, srv.ListenAndServe())
+}
